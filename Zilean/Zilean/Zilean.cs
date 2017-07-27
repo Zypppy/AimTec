26fa8f8593f4f774098f9226b1c3f3da91a908b7
Zypppy <zypppaliukas@gmail.com>
2017-07-27 20:52:05 +0300

    Updatink Zilean . Should Work . Only Q

--- Zilean/Zilean/Zilean.cs
+++ Zilean/Zilean/Zilean.cs
@@ -22,20 +22,19 @@
     internal class Zilean
     {
 
-        public static Menu Menu = new Menu("Lee Sin By Zypppy", "Lee Sin By Zypppy", true);
+        public static Menu Menu = new Menu("Zilean By Zypppy", "Zilean By Zypppy", true);
 
         public static Orbwalker Orbwalker = new Orbwalker();
 
         public static Obj_AI_Hero Player = ObjectManager.GetLocalPlayer();
 
-        public static Spell Q, Q2, W, W2, E, E2, R;
+        public static Spell Q, W, R;
 
         public void LoadSpells()
 
         {
-            Q = new Spell(SpellSlot.Q, 1000);
-            W = new Spell(SpellSlot.W, 700);
-            Q.SetSkillshot(0.25f, 60f, 1800f, true, SkillshotType.Circle, false, HitChance.High);
+            Q = new Spell(SpellSlot.Q, 850);
+            Q.SetSkillshot(0.650f, 150f, 1800f, false, SkillshotType.Circle, false, HitChance.High);
 
 
 
@@ -48,83 +47,22 @@
             var ComboMenu = new Menu("combo", "Combo");
             {
                 ComboMenu.Add(new MenuBool("useq", "Use Q"));
-                ComboMenu.Add(new MenuBool("useqq", "Use Second Q"));
-                ComboMenu.Add(new MenuBool("usew", "Use W"));
-                ComboMenu.Add(new MenuBool("useww", "Use Second W"));
-                ComboMenu.Add(new MenuBool("usee", "Use E"));
-                ComboMenu.Add(new MenuBool("useed", "Use Second E"));
             }
 
 
             Menu.Add(ComboMenu);
-            var KSMenu = new Menu("killsteal", "Killsteal");
-            {
-                KSMenu.Add(new MenuBool("kq", "Killsteal with Q "));
-                KSMenu.Add(new MenuBool("kr", "Killsteal with R "));
-            }
-            Menu.Add(KSMenu);
+            
             Menu.Attach();
 
             Game.OnUpdate += Game_OnUpdate;
 
             LoadSpells();
-            Console.WriteLine("Lee Sin by Zypppy - Loaded");
+            Console.WriteLine("Zilean by Zypppy - Loaded");
         }
 
 
 
-        static double GetR(Obj_AI_Base target)
-        {
-            double meow = 0;
-            if (Player.SpellBook.GetSpell(SpellSlot.R).Level == 1)
-            {
-                meow = 150;
-            }
-            if (Player.SpellBook.GetSpell(SpellSlot.R).Level == 2)
-            {
-                meow = 300;
-            }
-            if (Player.SpellBook.GetSpell(SpellSlot.R).Level == 3)
-            {
-                meow = 450;
-            }
-
-            double calc = (Player.TotalAttackDamage - Player.BaseAttackDamage) * 2.0;
-            double full = calc + meow;
-            double damage = Player.CalculateDamage(target, DamageType.Physical, full);
-            return damage;
-        }
-
-        static double GetQ(Obj_AI_Base target)
-        {
-            double meow = 0;
-            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 1)
-            {
-                meow = 50;
-            }
-            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 2)
-            {
-                meow = 80;
-            }
-            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 3)
-            {
-                meow = 110;
-            }
-            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 4)
-            {
-                meow = 140;
-            }
-            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 5)
-            {
-                meow = 170;
-            }
-
-            double calc = (Player.TotalAttackDamage - Player.BaseAttackDamage) * 0.9;
-            double full = calc + meow;
-            double damage = Player.CalculateDamage(target, DamageType.Physical, full);
-            return damage;
-        }
-
+        
 
 
 
@@ -150,8 +88,7 @@
             }
 
 
-
-            Killsteal();
+            
         }
 
 
@@ -164,36 +101,7 @@
         }
 
 
-        private void Killsteal()
-        {
-            var enemies = GetBestKillableHero(Q, DamageType.Physical, false);
-            if (enemies != null)
-            {
-                bool useRK = Menu["killsteal"]["kr"].Enabled;
-                if (useRK && (GetR(enemies) > enemies.Health || Player.GetSpellDamage(enemies, SpellSlot.R) > enemies.Health))
-                {
-
-                    if (Player.GetSpellDamage(enemies, SpellSlot.R) > GetR(enemies))
-                    {
-                        R.Cast(enemies);
-                    }
-
-                }
-                bool useQK = Menu["killsteal"]["kq"].Enabled;
-                if (useQK && (GetQ(enemies) > enemies.Health || Player.GetSpellDamage(enemies, SpellSlot.Q) > enemies.Health && Player.SpellBook.GetSpell(SpellSlot.Q).Name == "BlindMonkQOne"))
-                {
-
-                    if (Player.GetSpellDamage(enemies, SpellSlot.Q) > GetQ(enemies))
-                    {
-                        Q.Cast(enemies);
-                    }
-                }
-
-
-            }
-
-        }
-
+       
 
 
         public static Obj_AI_Hero GetBestEnemyHeroTarget()
@@ -224,11 +132,6 @@
         {
 
             bool useQ = Menu["combo"]["useq"].Enabled;
-            bool useQ2 = Menu["combo"]["useqq"].Enabled;
-            bool useW = Menu["combo"]["usew"].Enabled;
-            bool useW2 = Menu["combo"]["useww"].Enabled;
-            bool useE = Menu["combo"]["usee"].Enabled;
-            bool useE2 = Menu["combo"]["useed"].Enabled;
             var target = GetBestEnemyHeroTargetInRange(Q.Range);
 
             if (!target.IsValidTarget())
@@ -238,63 +141,14 @@
 
 
 
-            if (Q.Ready && useQ && Player.SpellBook.GetSpell(SpellSlot.Q).Name == "BlindMonkQOne" && target.IsValidTarget(Q.Range))
+            if (Q.Ready && useQ && target.IsValidTarget(Q.Range))
             {
 
                 if (target != null)
                 {
                     Q.Cast(target);
                 }
-            }
-            if (Q2.Ready && useQ2 && Player.SpellBook.GetSpell(SpellSlot.Q).Name == "BlindMonkQTwo" && target.IsValidTarget(Q2.Range) && target.HasBuff("BlindMonkQOne"))
-            {
-
-                if (target != null)
-                {
-                    DelayAction.Queue(3000, () =>
-                    {
-                        Q2.Cast();
-                    });
-                }
-            }
-            if (W.Ready && useW && Player.SpellBook.GetSpell(SpellSlot.W).Name == "BlindMonkWOne" && target.IsValidTarget(W.Range))
-            {
-
-                if (target != null)
-                {
-                    W.CastOnUnit(Player);
-                }
-            }
-            if (W2.Ready && useW2 && Player.SpellBook.GetSpell(SpellSlot.W).Name == "BlindMonkWTwo" && target.IsValidTarget(W2.Range))
-            {
-
-                if (target != null)
-                {
-                    {
-                        W2.Cast();
-                    }
-                }
-            }
-            if (E.Ready && useE && Player.SpellBook.GetSpell(SpellSlot.E).Name == "BlindMonkEOne" && target.IsValidTarget(E.Range))
-            {
-
-                if (target != null)
-                {
-                    E.Cast();
-                }
-            }
-            if (E2.Ready && useE2 && Player.SpellBook.GetSpell(SpellSlot.E).Name == "BlindMonkETwo" && target.IsValidTarget(E2.Range) && target.HasBuff("BlindMonkEOne"))
-            {
-
-                if (target != null)
-
-                {
-                    DelayAction.Queue(2500, () =>
-                    {
-                        E2.Cast();
-                    });
-                }
-            }
+            }     
 
         }
 

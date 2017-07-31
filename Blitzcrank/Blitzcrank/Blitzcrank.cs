6ea88f19c48f6c7ee0091306aee6ec27a8645a40
Zypppy <zypppaliukas@gmail.com>
2017-07-31 17:13:30 +0300

    Ayyy Hi Blitz

---
+++ Blitzcrank/Blitzcrank/Blitzcrank.cs
@@ -0,0 +1,275 @@
+﻿namespace Blitzcrank
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    using Aimtec;
+    using Aimtec.SDK.Damage;
+    using Aimtec.SDK.Extensions;
+    using Aimtec.SDK.Menu;
+    using Aimtec.SDK.Menu.Components;
+    using Aimtec.SDK.Orbwalking;
+    using Aimtec.SDK.TargetSelector;
+    using Aimtec.SDK.Util.Cache;
+    using Aimtec.SDK.Prediction.Skillshots;
+    using Aimtec.SDK.Util;
+    using Spell = Aimtec.SDK.Spell;
+
+    internal class Blitzcrank
+    {
+        public static Menu Menu = new Menu("Blitzcrank by Zypppy", "Blitzcrank by Zypppy");
+        public static Orbwalker Orbwalker = new Orbwalker();
+        public static Obj_AI_Hero Player = ObjectManager.GetLocalPlayer();
+        public static Spell Q, E, R;
+        public void LoadSpells()
+        {
+            Q = new Spell(SpellSlot.Q, 1100);
+            E = new Spell(SpellSlot.E, 125);
+            R = new Spell(SpellSlot.R, 600);
+            Q.SetSkillshot(0.25f, 70f, 1800f, true, SkillshotType.Line, false, HitChance.High);
+        }
+
+        public Blitzcrank()
+        {
+            Orbwalker.Attach(Menu);
+            var ComboMenu = new Menu("combo", "Combo");
+            {
+                ComboMenu.Add(new MenuBool("useq", "Use Q"));
+                ComboMenu.Add(new MenuBool("usee", "Use E"));
+            }
+            Menu.Add(ComboMenu);
+            var KSMenu = new Menu("killsteal", "Killsteal");
+            {
+                KSMenu.Add(new MenuBool("kq", "Killsteal with Q "));
+                KSMenu.Add(new MenuBool("kr", "Killsteal with R "));
+            }
+            Menu.Add(KSMenu);
+            var miscmenu = new Menu("misc", "Misc.");
+            {
+                miscmenu.Add(new MenuBool("autoq", "Auto Q on CC"));
+            }
+            Menu.Add(miscmenu);
+            var DrawMenu = new Menu("drawings", "Drawings");
+            {
+                DrawMenu.Add(new MenuBool("drawq", "Draw Q Range"));
+                DrawMenu.Add(new MenuBool("drawe", "Draw E Range"));
+                DrawMenu.Add(new MenuBool("drawr", "Draw R Range"));
+            }
+            Menu.Add(DrawMenu);
+            Menu.Attach();
+
+            Render.OnPresent += Render_OnPresent;
+            Game.OnUpdate += Game_OnUpdate;
+
+            LoadSpells();
+            Console.WriteLine("Blitzcrank by Zypppy - Loaded");
+        }
+
+        static double GetR(Obj_AI_Base target)
+        {
+            double meow = 0;
+            if (Player.SpellBook.GetSpell(SpellSlot.R).Level == 1)
+            {
+                meow = 250;
+            }
+            if (Player.SpellBook.GetSpell(SpellSlot.R).Level == 2)
+            {
+                meow = 375;
+            }
+            if (Player.SpellBook.GetSpell(SpellSlot.R).Level == 3)
+            {
+                meow = 500;
+            }
+
+            double calc = (Player.TotalAbilityDamage - Player.BaseAbilityDamage) * 1.0;
+            double full = calc + meow;
+            double damage = Player.CalculateDamage(target, DamageType.Magical, full);
+            return damage;
+        }
+        static double GetQ(Obj_AI_Base target)
+        {
+            double meow = 0;
+            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 1)
+            {
+                meow = 80;
+            }
+            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 2)
+            {
+                meow = 135;
+            }
+            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 3)
+            {
+                meow = 190;
+            }
+            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 4)
+            {
+                meow = 245;
+            }
+            if (Player.SpellBook.GetSpell(SpellSlot.Q).Level == 5)
+            {
+                meow = 300;
+            }
+
+            double calc = (Player.TotalAbilityDamage - Player.BaseAbilityDamage) * 1.0;
+            double full = calc + meow;
+            double damage = Player.CalculateDamage(target, DamageType.Magical, full);
+            return damage;
+        }
+       
+        private void Render_OnPresent()
+        {
+
+            if (Menu["drawings"]["drawq"].Enabled)
+            {
+                Render.Circle(Player.Position, Q.Range, 40, Color.CornflowerBlue);
+            }
+            if (Menu["drawings"]["drawe"].Enabled)
+            {
+                Render.Circle(Player.Position, E.Range, 40, Color.CornflowerBlue);
+            }
+            if (Menu["drawings"]["drawr"].Enabled)
+            {
+                Render.Circle(Player.Position, R.Range, 40, Color.Crimson);
+            }
+            
+        }
+
+        private void Game_OnUpdate()
+        {
+
+            if (Player.IsDead || MenuGUI.IsChatOpen())
+            {
+                return;
+            }
+
+
+            switch (Orbwalker.Mode)
+            {
+                case OrbwalkingMode.Combo:
+                    OnCombo();
+                    break;
+                case OrbwalkingMode.Mixed:
+                    break;
+                case OrbwalkingMode.Laneclear:
+                    break;
+
+            }
+            Killsteal();
+            if (Menu["misc"]["autoq"].Enabled)
+            {
+                foreach (var target in GameObjects.EnemyHeroes.Where(
+                    t => (t.HasBuffOfType(BuffType.Charm) || t.HasBuffOfType(BuffType.Stun) ||
+                          t.HasBuffOfType(BuffType.Fear) || t.HasBuffOfType(BuffType.Snare) ||
+                          t.HasBuffOfType(BuffType.Taunt) || t.HasBuffOfType(BuffType.Knockback) ||
+                          t.HasBuffOfType(BuffType.Suppression)) && t.IsValidTarget(Q.Range) &&
+                         !Invulnerable.Check(t, DamageType.Magical)))
+                {
+
+                    Q.Cast(target);
+                }
+            }
+        }
+
+
+
+
+        public static Obj_AI_Hero GetBestKillableHero(Spell spell, DamageType damageType = DamageType.True,
+            bool ignoreShields = false)
+        {
+            return TargetSelector.Implementation.GetOrderedTargets(spell.Range).FirstOrDefault(t => t.IsValidTarget());
+        }
+
+
+        private void Killsteal()
+        {
+            var enemies = GetBestKillableHero(Q, DamageType.Physical, false);
+            if (enemies != null)
+            {
+                bool useRK = Menu["killsteal"]["kr"].Enabled;
+                if (useRK && (GetR(enemies) > enemies.Health || Player.GetSpellDamage(enemies, SpellSlot.R) > enemies.Health))
+                {
+
+                    if (Player.GetSpellDamage(enemies, SpellSlot.R) > GetR(enemies))
+                    {
+                        R.Cast();
+                    }
+
+                }
+                bool useQK = Menu["killsteal"]["kq"].Enabled;
+                if (useQK && (GetQ(enemies) > enemies.Health || Player.GetSpellDamage(enemies, SpellSlot.Q) > enemies.Health))
+                {
+
+                    if (Player.GetSpellDamage(enemies, SpellSlot.Q) > GetQ(enemies))
+                    {
+                        Q.Cast(enemies);
+                    }
+                }
+
+
+            }
+
+        }
+        public static Obj_AI_Hero GetBestEnemyHeroTarget()
+        {
+            return GetBestEnemyHeroTargetInRange(float.MaxValue);
+        }
+
+        public static Obj_AI_Hero GetBestEnemyHeroTargetInRange(float range)
+        {
+            var ts = TargetSelector.Implementation;
+            var target = ts.GetTarget(range);
+            if (target != null && target.IsValidTarget() && !Invulnerable.Check(target))
+            {
+                return target;
+            }
+
+            var firstTarget = ts.GetOrderedTargets(range)
+                .FirstOrDefault(t => t.IsValidTarget() && !Invulnerable.Check(t));
+            if (firstTarget != null)
+            {
+                return firstTarget;
+            }
+
+            return null;
+        }
+
+        private void OnCombo()
+        {
+
+            bool useQ = Menu["combo"]["useq"].Enabled;
+            bool useE = Menu["combo"]["usee"].Enabled;
+            var target = GetBestEnemyHeroTargetInRange(Q.Range);
+
+            if (!target.IsValidTarget())
+            {
+                return;
+            }
+
+
+
+            if (Q.Ready && useQ && target.IsValidTarget(Q.Range))
+            {
+
+                if (target != null)
+                {
+                    Q.Cast(target);
+                }
+            }
+            if (E.Ready && useE && target.IsValidTarget(E.Range))
+            {
+
+                if (target != null)
+                {
+                    E.Cast();
+                }
+            }
+            
+
+        }
+
+    }
+}
+
+       

77856974af48ed55d99f5ad26ee5c895f90c3162
Zypppy <zypppaliukas@gmail.com>
2017-08-05 18:53:46 +0300

    Update R

--- Teemo/Teemo/Teemo.cs
+++ Teemo/Teemo/Teemo.cs
@@ -30,7 +30,7 @@
             Q = new Spell(SpellSlot.Q, 675);
             W = new Spell(SpellSlot.W, Player.AttackRange);
             E = new Spell(SpellSlot.E, Player.AttackRange);
-            R = new Spell(SpellSlot.R, 450f + 150f * Player.SpellBook.GetSpell(SpellSlot.R).Level);
+            R = new Spell(SpellSlot.R, 450f + 250f * Player.SpellBook.GetSpell(SpellSlot.R).Level);
             R.SetSkillshot(1.0f, 60f, float.MaxValue, false, SkillshotType.Circle, false, HitChance.Medium);
         }
 

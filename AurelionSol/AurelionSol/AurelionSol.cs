feaafeff8c3e13d34605a1249b684cb4d68097ba
Zypppy <zypppaliukas@gmail.com>
2017-08-30 12:16:07 +0300

    AurelionSol Release

---
+++ AurelionSol/AurelionSol/AurelionSol.cs
@@ -0,0 +1,219 @@
+﻿namespace AurelionSol
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    using Aimtec;
+    using Aimtec.SDK.Prediction.Health;
+    using Aimtec.SDK.Damage;
+    using Aimtec.SDK.Extensions;
+    using Aimtec.SDK.Menu;
+    using Aimtec.SDK.Menu.Components;
+    using Aimtec.SDK.Orbwalking;
+    using Aimtec.SDK.TargetSelector;
+    using Aimtec.SDK.Util.Cache;
+    using Aimtec.SDK.Prediction.Skillshots;
+    using Aimtec.SDK.Util;
+
+    using Spell = Aimtec.SDK.Spell;
+    using Aimtec.SDK.Events;
+
+    internal class AurelionSol
+    {
+        public static Menu Menu = new Menu("AurelionSol by Zypppy", "AurelionSol by Zypppy", true);
+        public static Orbwalker Orbwalker = new Orbwalker();
+        public static Obj_AI_Hero Player = ObjectManager.GetLocalPlayer();
+        public static Spell Q, W, W2, E, R;
+        private MissileClient missiles;
+
+        public void LoadSpells()
+        {
+            Q = new Spell(SpellSlot.Q, 650f);
+            W = new Spell(SpellSlot.W, 350f);
+            W2 = new Spell(SpellSlot.W, 600f);
+            E = new Spell(SpellSlot.E, 400f);
+            R = new Spell(SpellSlot.R, 1420f);
+            Q.SetSkillshot(0.25f, 180, 850, false, SkillshotType.Line);
+            R.SetSkillshot(0.5f, 300, 4500, false, SkillshotType.Line);
+        }
+        public AurelionSol()
+        {
+            Orbwalker.Attach(Menu);
+            var ComboMenu = new Menu("combo", "Combo");
+            {
+                ComboMenu.Add(new MenuBool("useq", "Use Q"));
+                ComboMenu.Add(new MenuBool("usew", "Use W"));
+                ComboMenu.Add(new MenuBool("user", "Use R"));
+                ComboMenu.Add(new MenuSlider("hitr", "R Minimum Enemeies Hit", 3, 1, 5));
+            }
+            Menu.Add(ComboMenu);
+            var HarassMenu = new Menu("harass", "Harass");
+            {
+                HarassMenu.Add(new MenuBool("useq", "Use Q"));
+                HarassMenu.Add(new MenuSlider("manaq", "Minimum Mana To Use Q", 70, 0, 100));
+                HarassMenu.Add(new MenuBool("usew", "Use W"));
+                HarassMenu.Add(new MenuSlider("manaw", "Minimum Mana To Use W", 70, 0, 100));
+            }
+            Menu.Add(HarassMenu);
+            var KillstealMenu = new Menu("killsteal", "Killsteal");
+            {
+                KillstealMenu.Add(new MenuBool("RKS", "Use R to Killsteal"));
+            }
+            Menu.Add(KillstealMenu);
+            var DrawingsMenu = new Menu("drawings", "Drawings");
+            {
+                DrawingsMenu.Add(new MenuBool("drawq", "Draw Q Range"));
+                DrawingsMenu.Add(new MenuBool("draww", "Draw W Range"));
+                DrawingsMenu.Add(new MenuBool("draww2", "Draw Active W Range"));
+                DrawingsMenu.Add(new MenuBool("drawr", "Draw R Range"));
+            }
+            Menu.Add(DrawingsMenu);
+            Menu.Attach();
+
+            Render.OnPresent += Render_OnPresent;
+            Game.OnUpdate += Game_OnUpdate;
+            GameObject.OnCreate += OnCreate;
+            GameObject.OnDestroy += OnDestroy;
+
+            LoadSpells();
+            Console.WriteLine("AurelionSol by Zypppy - Loaded");
+        }
+        public void OnCreate(GameObject obj)
+        {
+           if (obj != null && obj.IsValid)
+           {
+               Console.WriteLine(obj.Name);
+           }
+        }
+        private void OnDestroy(GameObject obj)
+        {
+            var missile = obj as MissileClient;
+            if (missile == null || !missile.IsValid)
+            {
+                return;
+            }
+
+            if (missile.SpellCaster == null || !missile.SpellCaster.IsValid ||
+                missile.SpellCaster.Team != ObjectManager.GetLocalPlayer().Team)
+            {
+                return;
+            }
+            var hero = missile.SpellCaster as Obj_AI_Hero;
+            if (hero == null)
+            {
+                return;
+            }
+            if (missile.SpellData.Name == "LissandraEMissile")
+            {
+                missiles = null;
+            }
+        }
+        private void Render_OnPresent()
+        {
+            Vector2 maybeworks;
+            var heropos = Render.WorldToScreen(Player.Position, out maybeworks);
+            var xaOffset = (int)maybeworks.X;
+            var yaOffset = (int)maybeworks.Y;
+
+            if (Q.Ready && Menu["drawings"]["drawq"].Enabled)
+            {
+                Render.Circle(Player.Position, Q.Range, 40, Color.Indigo);
+            }
+            if (W.Ready && Menu["drawings"]["draww2"].Enabled)
+            {
+                Render.Circle(Player.Position, W2.Range, 40, Color.Indigo);
+            }
+            if (W.Ready && Menu["drawings"]["draww"].Enabled)
+            {
+                Render.Circle(Player.Position, W.Range, 40, Color.Indigo);
+            }
+            if (R.Ready && Menu["drawings"]["drawr"].Enabled)
+            {
+                Render.Circle(Player.Position, R.Range, 40, Color.Indigo);
+            }
+        }
+        private void Game_OnUpdate()
+        {
+            if (Player.IsDead || MenuGUI.IsChatOpen())
+            {
+                return;
+            }
+            switch (Orbwalker.Mode)
+            {
+                case OrbwalkingMode.Combo:
+                    OnCombo();
+                    break;
+                case OrbwalkingMode.Mixed:
+                    //OnHarass();
+                    break;
+                case OrbwalkingMode.Laneclear:
+                    break;
+            }
+            Killsteal();
+        }
+        public static Obj_AI_Hero GetBestKillableHero(Spell spell, DamageType damageType = DamageType.True,
+            bool ignoreShields = false)
+        {
+            return TargetSelector.Implementation.GetOrderedTargets(spell.Range).FirstOrDefault(t => t.IsValidTarget());
+        }
+        private void Killsteal()
+        {
+            if (R.Ready && Menu["killsteal"]["RKS"].Enabled)
+            {
+                var besttarget = GetBestKillableHero(R, DamageType.Magical, false);
+                var RPrediction = R.GetPrediction(besttarget);
+                if (besttarget != null && Player.GetSpellDamage(besttarget, SpellSlot.R) >= besttarget.Health && besttarget.IsValidTarget(R.Range))
+                {
+                    if (RPrediction.HitChance >= HitChance.High)
+                    {
+                        R.Cast(RPrediction.CastPosition);
+                    }
+                }
+            }
+        }
+        public static Obj_AI_Hero GetBestEnemyHeroTarget()
+        {
+            return GetBestEnemyHeroTargetInRange(float.MaxValue);
+        }
+        public static Obj_AI_Hero GetBestEnemyHeroTargetInRange(float range)
+        {
+            var ts = TargetSelector.Implementation;
+            var target = ts.GetTarget(range);
+            if (target != null && target.IsValidTarget() && !Invulnerable.Check(target))
+            {
+                return target;
+            }
+            var firstTarget = ts.GetOrderedTargets(range)
+                .FirstOrDefault(t => t.IsValidTarget() && !Invulnerable.Check(t));
+            if (firstTarget != null)
+            {
+                return firstTarget;
+            }
+            return null;
+        }
+        private void OnCombo()
+        {
+            var target = GetBestEnemyHeroTargetInRange(R.Range);
+            bool useQ = Menu["combo"]["useq"].Enabled;
+            var QPrediction = Q.GetPrediction(target);
+            bool useW = Menu["combo"]["usew"].Enabled;
+            bool useR = Menu["combo"]["user"].Enabled;
+            float hitR = Menu["combo"]["hitr"].As<MenuSlider>().Value;
+            var RPrediction = R.GetPrediction(target);
+
+            if (!target.IsValidTarget())
+            {
+                return;
+            }
+            if (Q.Ready && target.IsValidTarget(Q.Range) && useQ)
+            {
+                if (QPrediction.HitChance >= HitChance.Medium)
+                {
+                    Q.Cast(QPrediction.CastPosition);
+                }
+            }
+        }
+    }
+}

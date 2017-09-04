99462929ff6aa07c888b6109d237d36cfaf82ec5
Zypppy <zypppaliukas@gmail.com>
2017-09-04 19:03:18 +0300

    update

--- KhaZix/KhaZix/KhaZix.cs
+++ KhaZix/KhaZix/KhaZix.cs
@@ -291,5 +291,25 @@
                 }
             }
         }
+        public static Obj_AI_Hero GetBestEnemyHeroTarget()
+        {
+            return GetBestEnemyHeroTargetInRange(float.MaxValue);
+        }
+        public static Obj_AI_Hero GetBestEnemyHeroTargetInRange(float range)
+        {
+            var ts = TargetSelector.Implementation;
+            var target = ts.GetTarget(range);
+            if (target != null && target.IsValidTarget() && !Invulnerable.Check(target))
+            {
+                return target;
+            }
+            var firstTarget = ts.GetOrderedTargets(range)
+                .FirstOrDefault(t => t.IsValidTarget() && !Invulnerable.Check(t));
+            if (firstTarget != null)
+            {
+                return firstTarget;
+            }
+            return null;
+        }
     }
 }

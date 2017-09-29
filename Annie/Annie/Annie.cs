32d4db3f115a7fedf460860783db282ce63a06fe
Zypppy <zypppaliukas@gmail.com>
2017-09-29 23:12:50 +0300

    Random

---
+++ Annie/Annie/Annie.cs
@@ -0,0 +1,53 @@
+﻿namespace Annie
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    using Aimtec;
+    using Aimtec.SDK.Prediction.Health;
+    using Aimtec.SDK.Damage;
+    using Aimtec.SDK.Extensions;
+    using Aimtec.SDK.Menu;
+    using Aimtec.SDK.Menu.Components;
+    using Aimtec.SDK.Orbwalking;
+    using Aimtec.SDK.TargetSelector;
+    using Aimtec.SDK.Util.Cache;
+    using Aimtec.SDK.Prediction.Skillshots;
+    using Aimtec.SDK.Util;
+
+    using Spell = Aimtec.SDK.Spell;
+    using Aimtec.SDK.Events;
+
+    internal class Annie
+    {
+        public static Menu Menu = new Menu("Annie by Zypppy", "Annie by Zypppy", true);
+        public static Orbwalker Orbwalker = new Orbwalker();
+        public static Obj_AI_Hero Player = ObjectManager.GetLocalPlayer();
+        public static Spell Q, W, E, R, Ignite;
+
+        public void LoadSpells()
+        {
+            Q = new Spell(SpellSlot.Q, 625f);
+            W = new Spell(SpellSlot.W, 600f);
+            W.SetSkillshot(0.50f, 250f, float.MaxValue, false, SkillshotType.Circle);
+            E = new Spell(SpellSlot.E);
+            R = new Spell(SpellSlot.R, 625f);
+            R.SetSkillshot(0.20f, 250f, float.MaxValue, false, SkillshotType.Circle);
+            if (Player.SpellBook.GetSpell(SpellSlot.Summoner1).SpellData.Name == "SummonerDot")
+                Ignite = new Spell(SpellSlot.Summoner1, 600);
+            if (Player.SpellBook.GetSpell(SpellSlot.Summoner2).SpellData.Name == "SummonerDot")
+                Ignite = new Spell(SpellSlot.Summoner2, 600);
+        }
+        public Annie()
+        {
+            Orbwalker.Attach(Menu);
+            var Combo = new Menu("combo", "Combo");
+            {
+
+            }
+            Menu.Add(Combo);
+        }
+    }
+}

1a716dd9fc9a74581c463592ce9a784b922a05c1
Zypppy <zypppaliukas@gmail.com>
2017-10-20 16:49:34 +0300

    Update

--- Lux/Lux/Lux.cs
+++ Lux/Lux/Lux.cs
@@ -280,9 +280,12 @@
                         }
                         break;
                     case 2:
-                        if (LuxE.CountEnemyHeroesInRange(335f) >= 1 && Player.SpellBook.GetSpell(SpellSlot.E).ToggleState == 2 && LuxE != null) 
+                        if (LuxE != null)
                         {
-                           E2.Cast();
+                            if (LuxE.CountEnemyHeroesInRange(335f) >= 1 && Player.SpellBook.GetSpell(SpellSlot.E).ToggleState == 2)
+                            {
+                                E2.Cast(target);
+                            }
                         }
                         break;
                 }

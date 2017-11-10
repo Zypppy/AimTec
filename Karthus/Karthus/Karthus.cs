b910fb473d81038ce7d6bf6bb3af6261e04fa9b9
Zypppy <zypppaliukas@gmail.com>
2017-11-10 20:54:48 +0200

    Update

--- Karthus/Karthus/Karthus.cs
+++ Karthus/Karthus/Karthus.cs
@@ -29,7 +29,7 @@
 
         public void LoadSpells()
         {
-            Q = new Spell(SpellSlot.Q, 950f);
+            Q = new Spell(SpellSlot.Q, 975f);
             Q.SetSkillshot(0.95f, 85f, float.MaxValue, false, SkillshotType.Circle);
             W = new Spell(SpellSlot.W, 1000f);
             W.SetSkillshot(0.5f, 50f, float.MaxValue, false, SkillshotType.Circle);
@@ -261,7 +261,7 @@
                         }
                         break;
                     case 1:
-                        if (target.IsValidTarget(E.Range + 150) && Player.ManaPercent() <= ME && Player.SpellBook.GetSpell(SpellSlot.E).ToggleState == 1)
+                        if (target.IsValidTarget(E.Range + 150) || Player.ManaPercent() <= ME && Player.SpellBook.GetSpell(SpellSlot.E).ToggleState == 1)
                         {
                             E.Cast();
                         }
